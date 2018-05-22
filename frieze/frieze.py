e0fd455f8463dd02baa87adcda74acbc78cfa055
xfmeng17 <xfmeng17@foxmail.com>
2018-05-22 19:22:23 +0800

    add vertical but not return correct answer

--- frieze/frieze.py
+++ frieze/frieze.py
@@ -2,12 +2,12 @@
 # -*- coding: utf8 -*-
 
 matrix = list()
-with open ('frieze_1.txt', 'r') as f:
+with open ('frieze_2.txt', 'r') as f:
 	for line in f.readlines():
 		line = line.strip()
 		if not len(line):
 			continue
-		matrix.append(line.split())
+		matrix.append(list(map(int, line.split())))
 
 def check_input(matrix):
 	height = len(matrix)
@@ -50,3 +50,62 @@ def check_column(matrix, columns, period, length):
 
 print(get_period(matrix))
 
+def vertical_at_axis(pat, x_axis):
+	height = len(pat)
+	length = len(pat[0])
+	offset = 1 if length % 2 == 0 else 0
+
+	for y_lf in range(0, height):
+		lf_most = (x_axis - (length // 2) + offset) % length
+		rt_most = length - 1 - lf_most
+		for x_lf in range(0, length):
+			x_rt = (x_lf + ((x_axis - x_lf) * 2) + offset) % length
+			y_rt = y_lf
+
+			# 0001 => |
+			if (pat[y_lf][x_lf] & 1):
+				if (not (pat[y_rt][x_rt] & 1)):
+					return False
+			# 0010 => /
+			elif (pat[y_lf][x_lf] & 2):
+				x_rt = x_rt - 1
+				y_rt = y_rt - 1
+				if (x_rt < lf_most or y_rt < 0):
+					return False
+				if (not (pat[y_rt][x_rt] & 8)):
+					return False
+			# 0100 => -
+			elif (pat[y_lf][x_lf] & 4):
+				x_rt = x_rt - 1
+				if (x_rt < lf_most):
+					return False
+				if (not (pat[y_rt][x_rt] & 4)):
+					return False
+			# 1000 => /
+			elif (pat[y_lf][x_lf] & 8):
+				x_rt = x_rt + 1
+				y_rt = y_rt + 1
+				if (x_rt > rt_most or y_rt > height - 1):
+					return False
+				if (not (pat[y_rt][x_rt] & 2)):
+					return False
+			# 0000 => blank
+			else:
+				if pat[y_rt][x_rt]:
+					return False
+
+	return True
+
+def vertical(pat):
+	for x_axis in range(0, len(pat[0])):
+		if (vertical_at_axis(pat, x_axis)):
+			return True
+	return False
+
+period = get_period(matrix)
+pattern = list()
+for row in matrix:
+	pattern.append(row[0: period])
+print(pattern)
+
+print(vertical(pattern))
