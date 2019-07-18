a088cb858448d69d93d3bcf5eb6735a62ebff256
xfmeng17 <xfmeng17@foxmail.com>
2019-07-18 17:53:29 +0800

    feat(cpp): add test/ directory

---
+++ cpp/test/test_float_str.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main(void) {
+	stringstream ss;
+	ss.clear();
+	double val1 = 612353.78901213456789;
+	ss << val1;
+	cout << "val1 = " << ss.str() << endl;
+	ss.clear();
+
+	string val2 = "612353e+005";
+	val1 = atof(val2.c_str());
+	cout << "val = " << val1 << endl;
+}
+
