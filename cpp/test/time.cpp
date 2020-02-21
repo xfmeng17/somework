ec8354a5c9dc4aa764fb27aadf15b7369e69e172
xfmeng17 <xfmeng17@foxmail.com>
2020-02-21 23:40:22 +0800

    feat(cpp): add string split implement

--- cpp/test/time.cpp
+++ cpp/test/time.cpp
@@ -2,14 +2,14 @@
 #include <time.h>
 
 int main(void) {
-	time_t now_time = time(NULL);
-	printf("now_time=%ld\n", now_time);
-	
-	struct tm tm_now_time;
-	localtime_r(&now_time, &tm_now_time);
-	int tm_now_hour = tm_now_time.tm_hour;
-	int tm_now_min = tm_now_time.tm_min;
-	printf("tm_now_hour=%d, tm_now_min=%d\n", tm_now_hour, tm_now_min);
+  time_t now_time = time(NULL);
+  printf("now_time=%ld\n", now_time);
 
-	return 0;
+  struct tm tm_now_time;
+  localtime_r(&now_time, &tm_now_time);
+  int tm_now_hour = tm_now_time.tm_hour;
+  int tm_now_min = tm_now_time.tm_min;
+  printf("tm_now_hour=%d, tm_now_min=%d\n", tm_now_hour, tm_now_min);
+
+  return 0;
 }
