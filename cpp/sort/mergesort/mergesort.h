5b138460a654e00d0ed8fd41a11f2dcd59a47767
xfmeng17 <xfmeng17@foxmail.com>
2020-04-19 21:46:42 +0800

    feat(cpp): update sort dir

---
+++ cpp/sort/mergesort/mergesort.h
@@ -0,0 +1,6 @@
+#ifndef _MERGESORT_H_
+#define _MERGESORT_H_
+
+int mergesort(int* arr, int lo, int hi);
+
+#endif  // _MERGESORT_H_
---
+++ cpp/sort/quicksort/quicksort.h
@@ -0,0 +1,6 @@
+#ifndef _QUICKSORT_H_
+#define _QUICKSORT_H_
+
+int quicksort(int* arr, int p, int q); 
+
+#endif //  _QUICKSORT_H_
