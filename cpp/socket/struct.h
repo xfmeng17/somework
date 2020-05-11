af59e4248458237122c456e7981f8eaeab525035
xfmeng17 <xfmeng17@foxmail.com>
2020-05-11 21:38:38 +0800

    uodate

---
+++ cpp/socket/struct.h
@@ -0,0 +1,13 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+typedef struct _UDP_MSG {
+  int add1;
+  int add2;
+  int sum;
+  char str1[16];
+  char str2[16];
+  char cat[32];
+} UDP_MSG;
+
+#endif /* STRUCT_H */
