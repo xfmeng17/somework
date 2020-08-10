0e8ead831b398ec2861d842ad5e4e6b91e355099
xfmeng17 <xfmeng17@foxmail.com>
2020-08-10 16:34:10 +0800

    2020-08-10 16:34:10

--- cpp/interview/test1.cpp
+++ cpp/interview/test1.cpp
@@ -5,6 +5,33 @@
 
 using namespace std;
 
+int func(vector<int> inputs, int N, int K) {
+	sort(inputs.begin(), inputs.end());
+	vector<int> seg;
+	int cur = inputs[0];
+	int cnt = 1;
+	seg.emplace_back(1);
+	for (int i = 1; i < inputs.size(); i++) {
+		if (inputs[i] - cur <= 2) {
+			seg[seg.size()-1]++;
+		} else {
+			seg.emplace_back(1);
+		}
+		cur = inputs[i];
+	}
+
+	for (int i = N/K; i >= 1; i--) {
+		int num = 0;
+		for (auto s : seg) {
+			num += s / i;
+		}
+		if (num >= K) {
+			return i;
+		}
+	}
+	return -1;
+}
+
 
 int main() {
   int T = 0;
@@ -13,16 +40,16 @@ int main() {
     int N = 0, K = 0;
     vector<int> inputs;
     cin >> N >> K;
-    if (N <= 0 || k <= 0) {
-        cout << -1 << end;
+    if (N <= 0 || K <= 0) {
+        cout << -1 << endl;
         continue;
     }
     for (int i = 0; i < N; i++) {
        int n = 0;
        cin >> n;
-       inputs.emplace_backn(n);
+       inputs.emplace_back(n);
     }
-    cout << func(input, N, K);
+    cout << func(inputs, N, K) << endl;
   }
 
   return 0;
