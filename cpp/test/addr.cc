435e1245d87c3302e826ca91c3712baf675ef8c9
xfmeng17 <xfmeng17@foxmail.com>
2021-04-22 20:43:58 +0800

    2021-04-22 20:43:58

--- cpp/test/addr.cc
+++ cpp/test/addr.cc
@@ -44,7 +44,7 @@ class T2 {
     double d;
     
     ~T2() {
-      std::cout << "c=" << c << ",c.length=" << c.length() << std::endl;
+      std::cout << "~T2(),c=" << c << ",c.length=" << c.length() << std::endl;
     }
 
   public:
@@ -65,29 +65,36 @@ void test2 () {
   std::cout << "t2 size=" << sizeof(t2) << std::endl;
 
   void* p = &t2;
-  std::cout << "p=" << (void*)p << std::endl;
-  std::cout << "p=" << (void*)((int*)p+1) <<  ",a=" << *((int*)p+1) << std::endl;
-  std::cout << "p=" << (void*)((char*)p+8) <<  ",b=" << *((char*)p+8) << std::endl;
+  std::cout << "t2_p=" << (void *)p << std::endl;
+  std::cout << "p10=" << (void*)((char*)p) <<  ",a=" << *((uint64_t*)p) << std::endl;
+  std::cout << "p20=" << (void*)((char*)p+8) <<  ",b=" << *((char*)p+8) << std::endl;
+
   void* p_str = ((char*)p)+16;
+  std::cout << "p_str(void*)=" << (void*)p_str << ",(uint64_t)=" << (uint64_t)p_str << std::endl; 
   uint64_t p_int = *((uint64_t*)(p_str));
-  std::cout << "p_str(uint64_t)=" << (uint64_t)p_str << ",(void*)=" << (void*)p_str << std::endl; 
-  std::cout << "p_int(uint64_t)=" << p_int << ",(void*)=" << (void*)p_int << std::endl;
+  std::cout << "p_int(void*)=" << (void*)p_int << ",(uint64_t)=" << (uint64_t)p_int << std::endl;
+
   char *p_char = (char*)p_int;
-  p_char[4] = '4';
-  p_char[5] = '5';
-  p_char[6] = '6';
-  p_char[7] = '7';
-  p_char[8] = '8';
+  std::cout << "p_char=" << (void *)p_char << std::endl;
+  p_char[0] = '0';
+  p_char[1] = '1';
+  p_char[2] = '2';
+  p_char[3] = '3';
+  // p_char[4] = '4';
+  // p_char[5] = '5';
+  // p_char[6] = '6';
+  // p_char[7] = '7';
   for (int i = 3; i < 1000; i++) {
-    p_char[i] = ('a' + i) % 100;
+    // p_char[i] = ('a' + i) % 100;
   }
 
-  std::cout << "p_char[8] addr=" << (void*)&(p_char[8]) << std::endl;
-  // std::cout << "p=" << (void*)((char*)p+16) <<  ",c=" << std::string(((char*)p_int), 16) << std::endl;
-  std::cout << "p=" << (void*)((char*)p+24) <<  ",d=" << *(double*)((char*)p+24) << std::endl;
+  std::cout << "p_char[0] addr=" << (void*)&(p_char[0]) << std::endl;
+  std::cout << "p30=" << (void *)((char *)p + 16) << ",t2.c_to_string=" << std::string(((char *)p_int), 10) << std::endl;
+  std::cout << "p40=" << (void*)((char*)p+24) <<  ",d=" << *(double*)((char*)p+24) << std::endl;
 }
 
 int main () {
+  // std::cout << "string sizeof =" << sizeof(std::string) << std::endl;
   test2();
   return 0;
 }
